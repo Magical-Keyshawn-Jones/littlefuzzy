fbe020b4a822d8f07c18fec7c206bcd4696ad562
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-04-02 00:59:54 -0500

    Added photos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
+import { useState } from 'react';
 import { Link, Route, Switch}from 'react-router-dom';
 import Gaming from './Gaming Components/Gaming';
 import Games from './Gaming Components/Games';
+// Goals: Add a video on my webpage/ Add moving png jpeg images on my webpage
 
 function App() {
-  // Add a onChange for nav navigation. Create a function that changes what the Nav, H1 says. Example "Welcome, to my homepage! Welcome, to my Favorite Games!"
-  
+  // Storing Welcome words in a Variable
+  const navWords = {
+    home: 'Welcome to my Home!',
+    gaming: 'Super Awesome Gaming Area!'
+  }
+
+  // Storing navWords in useState to be used in my nav
+  const [navWording, setNavWording] = useState(navWords.home)
+
+  // Returning my Website
   return (
    <div>
       <div className='Navigate'>
         <div className='Nav-Intro'>
-          <p>Welcome to My Home</p>
+          <p>{navWording}</p>
         </div>
         <nav>
-          <Link to='/' className='Link' id='HomeButton'>Home</Link>
-          <Link to='/gaming' className='Link'>Gaming</Link>
-          <Link to='/gaming/1'>Test</Link>
+          <Link to='/' className='Link' id='HomeButton' onClick={() => setNavWording(navWords.home)}>Home</Link>
+          <Link to='/gaming' className='Link' onClick={()=> {setNavWording(navWords.gaming)}}>Gaming</Link>
+          {/* <Link to='/gaming/1'>Test</Link>
           <Link to='/gaming/2'>Test</Link>
-          <Link to='/gaming/3'>Test</Link>
+          <Link to='/gaming/3'>Test</Link> */}
         </nav>
       </div>
 
