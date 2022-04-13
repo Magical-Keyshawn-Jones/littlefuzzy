1747cd19c79c43790045ac428307c3a9a9bdde69
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-04-13 14:13:42 -0500

    added sillyWords array

--- src/Form Components/Silly.js
+++ src/Form Components/Silly.js
@@ -1,11 +1,13 @@
 import sillyTester from "./SillyValidation";
+// Reference SillyForm Array to find Specific Words
+import { sillyWords } from "./sillyFormArrays";
 import * as yup from 'yup';
 import { useState } from 'react'
 
 // Make a cool slider
 // Dropdown options
 // Radio
-// Make a validation form
+// Customize to validation form 
 
 function Silly () {
 
@@ -27,39 +29,45 @@ function Silly () {
     function validator (name, value) {
         yup.reach(sillyTester, name)
         .validate(value)
-        .then(()=> setErrorValues({...errorValues, [name]: ''}))
-        .catch(err => setErrorValues({...errorValues, [err]: err.formError[0]}))
+        .then(()=> setErrorValues({...sillyValues, [name]: ''}))
+        .catch(err => setErrorValues({...sillyValues, [name]: err.errors[0]})) //err.errors is a completely unrelated variable. !Always! use errors to show error!
     }
 
     // change function for OnChangeHandler
     function change (name, value) {
 
+        setSillyValues({...sillyValues, [name]: value})
+
         // Validates the input
         validator(name, value)
-
-        setSillyValues({...sillyValues, [name]: value})
     }
 
     // onChange handler
     function onChangeHandler (event) {
         
         // Grabbing values
-        const {name, type, checked, value } = event.target
+        const { name, type, checked, value } = event.target
 
         // To grab correct checkbox values
         const checkboxValue = type === 'checkbox' ? checked : value
+
+        change(name, checkboxValue)
     }
+
+    console.log(sillyWords)
 
     return (
        <div className='SillyFormHolder'>
             <form className='SillyForm'>
                 <label>
                     Name
-                    <input
+                    <input 
+                    className= 'testInput'
                     type='text'
                     name='name'
                     placeholder='Type name here'
                     value={sillyValues.name}
+                    onChange={onChangeHandler}
                     />
                     {errorValues.name}
                 </label>
---
+++ src/Form Components/sillyFormArrays.js
@@ -0,0 +1,29 @@
+
+
+// Verb Generator for sillyParagraph
+export const movementVerbs = [
+    'bolted', 'bounced', 'cantered', 'charged', 'clambered', 'climbed',
+    'coasted', 'darted', 'dashed', 'drifted', 'clumped', 'crawled', 'crept',
+    'flown', 'frolicked', 'galloped', 'glided', 'hastened', 'hiked', 'hopped',
+    'hurried', 'hurtled', 'inched', 'jogged', 'journeyed', 'jumped', 'leaped',
+    'limped', 'lolloped', 'lumbered', 'lurched', 'marched', 'moseyed', 'paraded', 
+    'pranced', 'raced', 'rambled', 'roamed', 'ran', 'scampered', 'scooted', 'scrammed',
+    'scrambled', 'scurried', 'shuffled', 'skipped', 'sleepwalked', 'slid', 'slithered',
+    'sneaked', 'somersaulted', 'speeded', 'stomped', 'strolled', 'stumbled', 'swam', 
+    'tiptoed', 'traveled', 'trudged', 'vaulted', 'waddled', 'wandered', 'zigzagged', 'zoomed'
+]
+
+export const person = [
+ 'siblings', 'relative', 'Nuclear family', 'Foster Family', 'In-laws',
+ 'Grandfather', 'Grandmother', 'Father', 'Mother', 'Uncle', 'Aunt', 
+ 'Sister-in-Law', 'Brother', 'Sister', 'Cousin' , 'Nephew', 'Niece', 
+ 'Son', 'Grandson', 'Granddaughter', 'Daughter-in-Law', 'Stepmother', 
+ 'Stepson', 'Stepdaughter', 'Stepsister', 'Stepbrother', 'Halt-brother', 
+ 'Half-sister', 'Grandparents', 'Children', 'Brother-in-Law', 'Stepfather'   
+]
+
+export const sillyWords = [
+  person, movementVerbs
+]
+
+// console.log(sillyWords)
