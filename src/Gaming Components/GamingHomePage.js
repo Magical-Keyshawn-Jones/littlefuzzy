383776cf9c0cfc5cb38fc75dfd5df68e00df93fd
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-08-08 16:12:06 -0500

    Gaming/Deleted all but GamingHome css styles

--- src/Gaming Components/GamingHomePage.js
+++ src/Gaming Components/GamingHomePage.js
@@ -18,31 +18,6 @@ export default function GamingHomePage() {
 
   return (
     <div className="GamingHome">
-        {/* <div className='GamingTitle'>
-            <h1>I love Gaming and {words} Yall!!!</h1>
-        </div> */}
-
-      <div className="gamingContainer">
-        <div className="slideShowContainer">
-          <div id='arrow' className='arrowLeft'></div>
-          {/* <div className='backgroundArrow'></div> */}
-          <div className="slide">
-            <img src={gamingImages[current]} alt="Gaming Slide Show" />
-          </div>
-          {/* <div className='backgroundArrow'></div> */}
-          <div id='arrow' className='arrowRight'></div>
-        </div>
-        <div>
-          <p>
-            Insert form here. Inputs: game(required), rating(required), platform(required),
-            and comments(required)
-          </p> 
-        </div>
-      </div>
-
-      <div className="gamingReviews">
-        <h1> Testing Space </h1>
-      </div>
     </div>
   );
 }
